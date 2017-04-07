22a81a22ae2ba9f5b1e4fb1520bffbbd2e223b97
Jimmy Huang <jimminh@163.com>
2017-04-07 20:26:47 +0800

    patcher

---
+++ update-hash.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+CUR_DIR=$(dirname $(realpath $0))
+
+if [ $# -lt 1 ];then
+  echo "usage $0 ./pathto/atlassian-xxx.zip [ckdir] [./pathto/mysql-connector-java-x.x.x-bin.jar] [LANGDIR]"
+  exit 1
+fi
+ARCHIVE=$(realpath $1)
+
+if [ $# -gt 1 ];then
+   CKDIR=$(realpath $2)
+  else 
+   CKDIR=$CUR_DIR/xx
+fi
+echo "CKDIR=$CKDIR"
+if [ $# -gt 2 ];then
+    MYSQL_JDBC_FROM=$(realpath $3)
+fi
+if [ $# -gt 3 ];then
+    LANGDIR=$(realpath $3)
+   
+fi
+
+
+DATESTR=$(realpath $(date  +%Y%m%d-%Hh%Mm%Ss))
+mkdir -p $DATESTR
+###################################################################################################################
+# patch and update hash
+###################################################################################################################
+
+
+
+HAVE_HASH_FILE=0
+
+ATLASSIAN_EXTRAS=`unzip -l $ARCHIVE | grep "atlassian-extras-[0-9]" | awk '{print $4}'`
+ATLASSIAN_UNIVERSAL=`unzip -l $ARCHIVE | grep atlassian-universal-plugin-manager-plugin- | awk '{print $4}'`
+ATLASSIAN_EXTRAS_DECODER=`unzip -l $ARCHIVE | grep "atlassian-extras-decoder-[0-9v]" | awk '{print $4}'`
+
+ATLASSIAN_HASH_FILES=`unzip -l $ARCHIVE | grep "hash-registry.properties" | awk '{print $4}'`
+pushd $DATESTR
+
+if [[ ! -z $ATLASSIAN_HASH_FILES ]];then
+	HAVE_HASH_FILE=1
+fi
+if [[ HAVE_HASH_FILE -ne 0 ]];then
+	unzip $ARCHIVE $ATLASSIAN_HASH_FILES -d $DATESTR
+	cp $DATESTR/$ATLASSIAN_HASH_FILES $DATESTR/$ATLASSIAN_HASH_FILES.origin
+    if [[ ! -z $ATLASSIAN_EXTRAS ]];then
+    #    echo "$ATLASSIAN_EXTRAS"
+	    unzip $ARCHIVE $ATLASSIAN_EXTRAS -d $DATESTR
+		cp $DATESTR/$ATLASSIAN_EXTRAS $DATESTR/$ATLASSIAN_EXTRAS.origin
+		cp -v $CKDIR/$(basename $ATLASSIAN_EXTRAS) $DATESTR/$ATLASSIAN_EXTRAS
+		if [[ $? -ne 0 ]];then  exit $?;fi
+		zip -o $ARCHIVE $ATLASSIAN_EXTRAS
+		
+        ATLASSIAN_EXTRAS_MD5=`unzip  -p $ARCHIVE  $ATLASSIAN_EXTRAS | md5sum | awk '{print $1}'`
+    	ATLASSIAN_EXTRAS_MD5_LINE="fs.WEB-INF/lib/`basename $ATLASSIAN_EXTRAS`"
+    	echo "$ATLASSIAN_EXTRAS_MD5_LINE=$ATLASSIAN_EXTRAS_MD5"
+		perl  -n -e "print if m{^$ATLASSIAN_EXTRAS_MD5_LINE.+}" $DATESTR/$ATLASSIAN_HASH_FILES
+		perl -i -n -e "s@^$ATLASSIAN_EXTRAS_MD5_LINE.+@$ATLASSIAN_EXTRAS_MD5_LINE=$ATLASSIAN_EXTRAS_MD5@;print" $DATESTR/$ATLASSIAN_HASH_FILES
+    fi
+    
+    if [[ ! -z $ATLASSIAN_UNIVERSAL ]];then
+    #    echo "$ATLASSIAN_UNIVERSAL"
+		unzip $ARCHIVE $ATLASSIAN_UNIVERSAL -d $DATESTR
+		cp $DATESTR/$ATLASSIAN_UNIVERSAL $DATESTR/$ATLASSIAN_UNIVERSAL.origin
+		cp -v $CKDIR/$(basename $ATLASSIAN_UNIVERSAL) $DATESTR/$ATLASSIAN_UNIVERSAL;
+		if [[ $? -ne 0 ]];then exit $?;fi
+		zip -o $ARCHIVE $ATLASSIAN_UNIVERSAL
+		
+        ATLASSIAN_UNIVERSAL_MD5=`unzip  -p $ARCHIVE  $ATLASSIAN_UNIVERSAL | md5sum | awk '{print $1}'`
+    	ATLASSIAN_UNIVERSAL_MD5_LINE="fs.WEB-INF/atlassian-bundled-plugins/`basename $ATLASSIAN_UNIVERSAL`"
+    	echo "$ATLASSIAN_UNIVERSAL_MD5_LINE=$ATLASSIAN_UNIVERSAL_MD5"
+		perl  -n -e "print if m{^$ATLASSIAN_UNIVERSAL_MD5_LINE.+}" $DATESTR/$ATLASSIAN_HASH_FILES
+		perl -i -n -e "s@^$ATLASSIAN_UNIVERSAL_MD5_LINE.+@$ATLASSIAN_UNIVERSAL_MD5_LINE=$ATLASSIAN_UNIVERSAL_MD5@;print" $DATESTR/$ATLASSIAN_HASH_FILES
+    fi
+    
+    if [[ ! -z $ATLASSIAN_EXTRAS_DECODER ]];then
+    #    echo "$ATLASSIAN_EXTRAS_DECODER"
+		unzip $ARCHIVE $ATLASSIAN_EXTRAS_DECODER -d $DATESTR
+		cp $DATESTR/$ATLASSIAN_EXTRAS_DECODER $DATESTR/$ATLASSIAN_EXTRAS_DECODER.origin
+		cp $DATESTR/$ATLASSIAN_EXTRAS_DECODER $DATESTR/$ATLASSIAN_EXTRAS_DECODER.origin
+		cp -v $CKDIR/$(basename $ATLASSIAN_EXTRAS_DECODER) $DATESTR/$ATLASSIAN_EXTRAS_DECODER;
+		if [[ $? -ne 0 ]];then exit $?;fi
+		zip -o $ARCHIVE $ATLASSIAN_EXTRAS_DECODER
+		
+        ATLASSIAN_EXTRAS_DECODER_MD5=`unzip  -p $ARCHIVE  $ATLASSIAN_EXTRAS_DECODER | md5sum | awk '{print $1}'`
+    	ATLASSIAN_EXTRAS_DECODER_MD5_LINE="fs.WEB-INF/lib/`basename $ATLASSIAN_EXTRAS_DECODER`"
+    	echo "$ATLASSIAN_EXTRAS_DECODER_MD5_LINE=$ATLASSIAN_EXTRAS_DECODER_MD5"
+		perl  -n -e "print if m{^$ATLASSIAN_EXTRAS_DECODER_MD5_LINE.+}" $DATESTR/$ATLASSIAN_HASH_FILES
+		perl -i -n -e "s@^$ATLASSIAN_EXTRAS_DECODER_MD5_LINE.+@$ATLASSIAN_EXTRAS_DECODER_MD5_LINE=$ATLASSIAN_EXTRAS_DECODER_MD5@;print" $DATESTR/$ATLASSIAN_HASH_FILES
+    fi
+	zip -o $ARCHIVE $ATLASSIAN_HASH_FILES
+	popd
+
+fi
+
+
+###################################################################################################################
+#update db driver
+###################################################################################################################
+
+pushd $DATESTR
+
+#mysql-connector-java-5.1.40-bin.jar
+MYSQL_JDBC_HAVE=`unzip -l $ARCHIVE | egrep "mysql-connector-java-[0-9.]+-bin.jar" | awk '{print $4}'`
+if [[ -z $MYSQL_JDBC_HAVE && -n $MYSQL_JDBC_FROM ]];then
+      HSQLDB_JDBC=`unzip -l $ARCHIVE | egrep "hsqldb-[0-9.]+jar" | awk '{print $4}'`
+	  MYSQL_JDBC_TO=$(dirname $HSQLDB_JDBC)/$(basename $MYSQL_JDBC_FROM)
+	  # no mysql jdbc, form path
+      unzip $ARCHIVE $HSQLDB_JDBC -d $DATESTR
+	  cp -v $MYSQL_JDBC_FROM $MYSQL_JDBC_TO
+	  if [[ $? -ne 0 ]];then exit $?;fi
+      zip -o $ARCHIVE $MYSQL_JDBC_TO
+fi
+popd
+
+###################################################################################################################
+# filename space to _,  and add lang 
+###################################################################################################################
+pushd $DATESTR
+if [[ -n $LANGDIR ]];then
+    pushd $LANGDIR
+    find $LANGDIR -maxdepth 1 -name "* *"|
+    while read name;do
+            na=$(echo $name | tr ' ' '_')
+            mv "$name" $na
+    done
+    popd
+fi
+
+
+popd
+rm -rf $DATESTR
